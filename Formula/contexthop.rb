32b9d2eeb0a7b39c510abcec1ac75cc2345e07ae
Infurio <6320267+infurio@users.noreply.github.com>
2026-09-11 20:47:55 +0000

    Update contexthop to v0.7.4

--- Formula/contexthop.rb
+++ Formula/contexthop.rb
@@ -1,15 +1,15 @@
 class Contexthop < Formula
   desc "Isolated terminal contexts for Google Cloud, Kubernetes, and Docker"
   homepage "https://github.com/infurio/homebrew-tap"
-  version "0.7.3"
+  version "0.7.4"
   license "MIT"
   depends_on :macos
   depends_on arch: :arm64
 
   on_macos do
     on_arm do
-      url "https://github.com/infurio/homebrew-tap/releases/download/v0.7.3/contexthop_0.7.3_darwin_arm64.tar.gz"
-      sha256 "ea1e61332aab5bda3506478aff0b35a4512797177c2a74f7e241c175b7f86e41"
+      url "https://github.com/infurio/homebrew-tap/releases/download/v0.7.4/contexthop_0.7.4_darwin_arm64.tar.gz"
+      sha256 "064cc35a0600c45a7305837746fb6edc51cb5f45709617e336dead433587251a"
     end
   end
 
