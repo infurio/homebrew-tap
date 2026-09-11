f21a5ae62eed6a0e061e02b20a6a2cba3a790625
Infurio <6320267+infurio@users.noreply.github.com>
2026-09-11 17:37:27 -0400

    Update contexthop to v0.7.5

--- Formula/contexthop.rb
+++ Formula/contexthop.rb
@@ -1,17 +1,12 @@
 class Contexthop < Formula
   desc "Isolated terminal contexts for Google Cloud, Kubernetes, and Docker"
-  homepage "https://github.com/infurio/homebrew-tap"
-  version "0.7.4"
+  homepage "https://github.com/infurio/contexthop"
+  version "0.7.5"
   license "MIT"
   depends_on :macos
   depends_on arch: :arm64
-
-  on_macos do
-    on_arm do
-      url "https://github.com/infurio/homebrew-tap/releases/download/v0.7.4/contexthop_0.7.4_darwin_arm64.tar.gz"
-      sha256 "064cc35a0600c45a7305837746fb6edc51cb5f45709617e336dead433587251a"
-    end
-  end
+  url "https://github.com/infurio/homebrew-tap/releases/download/v0.7.5/contexthop_0.7.5_darwin_arm64.tar.gz"
+  sha256 "794e3c0a4912326608118eee753be3ec237e7c88af95bb870a28098cbc28d465"
 
   def install
     bin.install "chop"
